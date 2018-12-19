dc7d161a2299e4093717c924ebe3cccba89f2252
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2018-12-19 12:29:00 -0500

    Initial Commit

---
+++ ListMachine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListMachine
+//
+//  Created by Drew Lanning on 12/19/18.
+//  Copyright © 2018 Drew Lanning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
