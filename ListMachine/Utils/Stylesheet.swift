7b69631ed4422aa5f42c460e8bb715fd7aca4f91
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2019-01-21 09:28:35 -0500

    fleshed out style sheet a bit with color and font methods

--- ListMachine/Utils/Stylesheet.swift
+++ ListMachine/Utils/Stylesheet.swift
@@ -6,9 +6,94 @@
 //  Copyright © 2019 Drew Lanning. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum UserContentFeature {
+  case mainListCell, itemListCellTitle, itemListCellData, fieldListCell, userInput
+}
+
+enum SystemContentFeature {
+  case buttonLabel, navigationHeading, alertTitle, alertMessage, fieldLabel, pickerItem, segmentItem
+}
+
+enum ColorCategory {
+  case primary, secondary, accent, white, black
+}
 
 struct Stylesheet {
+  
+  // MARK: Colors
+  static func getColor(for category: ColorCategory) -> UIColor {
+    switch category {
+    case .accent:
+      return UIColor.init(red: 122/255, green: 59/255, blue: 105/255, alpha: 1.0)
+    case .black:
+      return UIColor.init(red: 41/255, green: 31/255, blue: 30/255, alpha: 1.0)
+    case .primary:
+      return UIColor.init(red: 71/255, green: 121/255, blue: 152/255, alpha: 1.0)
+    case .secondary:
+      return UIColor.init(red: 128/255, green: 155/255, blue: 206/255, alpha: 1.0)
+    case .white:
+      return UIColor.init(red: 234/255, green: 234/255, blue: 234/255, alpha: 1.0)
+    }
+  }
+  
+  // MARK: Fonts
+  static var systemFontName: String = "Montserrat"
+  static var systemFontBoldName: String = "Montserrat-Bold"
+  static var userFontName: String = "Open Sans"
+  
+  static func uiElementFont(for feature: SystemContentFeature) -> UIFont {
+    let size = getSize(for: feature)
+    let fontName: String
+    if feature == .navigationHeading {
+      fontName = systemFontBoldName
+    } else {
+      fontName = systemFontName
+    }
+    return UIFont(name: fontName, size: size) ?? UIFont.systemFont(ofSize: size)
+  }
+  
+  static func userContentFont(for feature: UserContentFeature) -> UIFont {
+    let size = getSize(for: feature)
+    return UIFont(name: userFontName, size: size) ?? UIFont.systemFont(ofSize: size)
+  }
+  
+  private static func getSize(for feature: UserContentFeature) -> CGFloat {
+    switch feature {
+    case .mainListCell:
+      return 20.0
+    case .itemListCellTitle:
+      return 18.0
+    case .itemListCellData:
+      return 14.0
+    case .fieldListCell:
+      return 20.0
+    case .userInput:
+      return 16.0
+    }
+  }
+  
+  private static func getSize(for feature: SystemContentFeature) -> CGFloat {
+    switch feature {
+    case .buttonLabel:
+      return 17.0
+    case .navigationHeading:
+      return 34.0
+    case .alertTitle:
+      return 18.0
+    case .alertMessage:
+      return 14.0
+    case .pickerItem:
+      return 23.0
+    case .segmentItem:
+      return 13.0
+    case .fieldLabel:
+      return 13.0
+    }
+  }
+  
+  // MARK: Date and time string formatting
   
   static private var simpleDateFormatter: DateFormatter {
     get {
