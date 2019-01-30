b339e7f09c99dd163b4a2e94665a5fb80d0d88c7
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2019-01-30 09:18:13 -0500

    basic show.hide for search bar and sort select bar

--- ListMachine/Controller/ItemListVC.swift
+++ ListMachine/Controller/ItemListVC.swift
@@ -14,6 +14,11 @@ class ItemListVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
   @IBOutlet weak var tableView: UITableView!
   @IBOutlet weak var searchBar: UISearchBar!
   @IBOutlet weak var sortSelect: SortSelectControl!
+  @IBOutlet weak var searchBtn: UIBarButtonItem!
+  @IBOutlet weak var sortBtn: UIBarButtonItem!
+  @IBOutlet weak var searchBarHeight: NSLayoutConstraint!
+  @IBOutlet weak var sortSelectHeight: NSLayoutConstraint!
+  
   var itemList: List!
   var store: DataStore?
   var sortKey: Int?
@@ -23,6 +28,9 @@ class ItemListVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
     }
   }
   var isSearching = false
+  
+  let revealedSortHeight: CGFloat = 27.0
+  let revealedSearchHeight: CGFloat = 56.0
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -43,6 +51,9 @@ class ItemListVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
     view.backgroundColor = Stylesheet.getColor(.white)
     tableView.backgroundColor = .clear
     searchBar.tintColor = Stylesheet.getColor(.primary)
+    searchBar.showsCancelButton = true
+    hideSearchBar()
+    hideSortSelect()
   }
   
   // MARK: Helper Methods
@@ -64,6 +75,28 @@ class ItemListVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
   func sortList(by sortKey: Int) {
     self.sortKey = sortKey
     tableView.reloadData()
+  }
+  
+  func showSearchBar() {
+    searchBarHeight.constant = revealedSearchHeight
+    searchBar.alpha = 1.0
+  }
+  
+  func hideSearchBar() {
+    searchBarHeight.constant = 0
+    searchBar.alpha = 0.0
+    searchBar.text = ""
+    stopSearching()
+  }
+  
+  func showSortSelect() {
+    sortSelectHeight.constant = revealedSortHeight
+    sortSelect.alpha = 1.0
+  }
+  
+  func hideSortSelect() {
+    sortSelectHeight.constant = 0
+    sortSelect.alpha = 0.0
   }
   
   // MARK: Tableview Methods
@@ -116,6 +149,22 @@ class ItemListVC: UIViewController, UITableViewDelegate, UITableViewDataSource, 
   
   @IBAction func sortSelected(sender: UISegmentedControl) {
     sortList(by: sender.selectedSegmentIndex)
+  }
+  
+  @IBAction func searchPressed(sender: UIBarButtonItem!) {
+    if searchBarHeight.constant == revealedSearchHeight {
+      hideSearchBar()
+    } else if searchBarHeight.constant == 0 {
+      showSearchBar()
+    }
+  }
+  
+  @IBAction func sortPressed(sender: UIBarButtonItem!) {
+    if sortSelectHeight.constant == revealedSortHeight {
+      hideSortSelect()
+    } else if sortSelectHeight.constant == 0 {
+      showSortSelect()
+    }
   }
   
   // MARK: Item Save Delegate
@@ -211,6 +260,7 @@ extension ItemListVC: UISearchBarDelegate {
   }
   
   func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.text = ""
     stopSearching()
   }
   
@@ -223,7 +273,6 @@ extension ItemListVC: UISearchBarDelegate {
       stopSearching()
     } else {
       filterString = searchText
-      print(filterString)
     }
   }
   
