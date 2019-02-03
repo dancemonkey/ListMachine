0cf6c57e6e49c873bcb96fc6d9b14a0c83258416
Drew Lanning <drewLanning@drews-macbook-air-2.local>
2019-02-03 09:40:36 -0500

    removed some comments and print statements, dead code

--- ListMachine/Utils/PopupFactory.swift
+++ ListMachine/Utils/PopupFactory.swift
@@ -53,44 +53,4 @@ class PopupFactory {
     return controller
   }
   
-//  internal func style(popup: PMAlertController) {
-//    popup.view.backgroundColor = Stylesheet.getColor(.white)
-//    popup.alertTitle.font = Stylesheet.uiElementFont(for: .alertTitle)
-//    for field in popup.textFields {
-//      field.font = Stylesheet.uiElementFont(for: .textField)
-//      field.textColor = Stylesheet.getColor(.black)
-//    }
-//  }
-  
-//  static func newListNamePopup(completion: @escaping () -> ()) -> NewListNameAlert {
-//    let controller = NewListNameAlert(title: "Name your list", message: nil, preferredStyle: .alert)
-//    controller.addTextField { (nameField) in
-//      nameField.placeholder = "Enter list name"
-//      nameField.font = Stylesheet.uiElementFont(for: .textField)
-//      let heightConstraint = NSLayoutConstraint(item: nameField, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 35.0)
-//      nameField.addConstraint(heightConstraint)
-//    }
-//
-//    let doneAction = UIAlertAction(title: "DONE", style: .default) { (action) in
-//      guard let text = controller.textFields![0].text else {
-//        controller.dismiss(animated: true, completion: nil)
-//        return
-//      }
-//      DataStore()?.save(object: List(name: text), andRun: nil)
-//      completion()
-//      controller.dismiss(animated: true, completion: nil)
-//    }
-//    let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
-//      controller.dismiss(animated: true, completion: nil)
-//    }
-//
-//    controller.addAction(doneAction)
-//    controller.addAction(cancelAction)
-//    controller.modalPresentationStyle = .overFullScreen
-//    controller.modalPresentationCapturesStatusBarAppearance = true
-//    controller.setNeedsStatusBarAppearanceUpdate()
-//
-//    return controller
-//  }
-  
 }
